2bf3c636f38feddd69e2e25374a00eece139e507
Tom Morgan <thomas.morgan1903@gmail.com>
2022-01-23 23:18:51 +0000

    added routes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const db = require('./db');
 require('dotenv/config');
 
+// route imports
+const projectRoutes = require('./routes/projectRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,11 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello');
 });
+app.use('/project', projectRoutes);
+app.use('/task', taskRoutes);
 
-app.get('/user', async (req, res) => {
-  const { rows } = await db.query('SELECT * FROM "User"');
-  res.json(rows);
-});
+// app.get('/user', async (req, res) => {
+//   const { rows } = await db.query('SELECT * FROM "User"');
+//   res.json(rows);
+// });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
